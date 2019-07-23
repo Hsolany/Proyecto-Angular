980ade0e18cd88b29657aa2b221460e127103126
unknown <haroldsolany@hotmail.com>
2019-07-22 21:08:22 -0500

    componente para aprobar credito

--- client/src/app/components/credito/credito.component.ts
+++ client/src/app/components/credito/credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { strictEqual } from 'assert';
 
 @Component({
   selector: 'app-credito',
@@ -22,6 +23,16 @@ export class CreditoComponent implements OnInit {
   }
 
   validarCredito(){
+    var val =this.credito.fecha.split("-");
+    var ano = Number(val[0]);
+    var mes = Number(val[1]);
+    var dia = Number(val[2]);
+    var hoy = new Date();
+  
+    var ano_hoy = hoy.getFullYear();
+    var mes_hoy= hoy.getMonth()+1;
+    var dia_hoy=hoy.getDay();
+    console.log('');
     if(this.credito.nit =='') {
       alert('Debe llenar el campo Nit');
       return false;
@@ -37,8 +48,11 @@ export class CreditoComponent implements OnInit {
     }else if(!/^([0-9])*$/.test(this.credito.nit)){
       alert('El campo Cedula solo acepta numeros ');
       return false;
-    }else if(this.credito.salario<100000000 || this.credito.salario<0 || this.credito.salario % 1 != 0){
+    }else if(this.credito.salario>100000000 || this.credito.salario<0 || this.credito.salario % 1 != 0){
       alert('La cantidad salarial es invalida');
+      return false;
+    }else if(ano==ano_hoy && mes==mes_hoy && dia==dia_hoy){
+      alert('No puede acceder al credito, no cumple los requitos');
       return false;
     }
   }
